fc582cf24c8cdf51138afd7b75d950fc68c07343
fengbo.hit <fengbo.hit@bytedance.com>
2020-07-02 17:48:34 +0800

    update

--- main.cpp
+++ main.cpp
@@ -3,15 +3,26 @@
 #include "ray.h"
 
 double hit_sphere(const point3& center, double radius, const ray& r){
+//    vec3 oc = r.origin() - center;
+//    auto a = dot(r.direction(), r.direction());
+//    auto b = 2.0 * dot(oc, r.direction());
+//    auto c = dot(oc, oc) - radius*radius;
+//    auto discriminant = b*b - 4*a*c;
+//    if (discriminant < 0) {
+//        return -1.0;
+//    } else {
+//        return (-b - sqrt(discriminant) ) / (2.0*a);
+//    }
+
     vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
     if (discriminant < 0) {
         return -1.0;
     } else {
-        return (-b - sqrt(discriminant) ) / (2.0*a);
+        return (-half_b - sqrt(discriminant) ) / a;
     }
 }
 
