e23896e9641d0faa9efb21671cd1fbfb4f49647d
fengbo.hit <fengbo.hit@bytedance.com>
2020-07-04 18:06:11 +0800

    10.3

--- main.cpp
+++ main.cpp
@@ -52,9 +52,11 @@ int main() {
     hittable_list world;
     world.add(std::make_shared<sphere>(point3(0,0,-1), 0.5, make_shared<lambertian>(color(0.7, 0.3, 0.3))));
     world.add(std::make_shared<sphere>(point3(0,-100.5,-1), 100, make_shared<lambertian>(color(0.8, 0.8, 0.0))));
-    world.add(std::make_shared<sphere>(point3(1,0,-1), 0.5, make_shared<metal>(color(.8,.6,.2), 0.3f)));
-    world.add(std::make_shared<sphere>(point3(-1,0,-1), 0.5, make_shared<metal>(color(.8,.8,.8), 1.0f)));
+//    world.add(std::make_shared<sphere>(point3(1,0,-1), 0.5, make_shared<metal>(color(.8,.6,.2), 0.3f)));
+//    world.add(std::make_shared<sphere>(point3(-1,0,-1), 0.5, make_shared<metal>(color(.8,.8,.8), 1.0f)));
 
+    world.add(make_shared<sphere>(point3(1,0,-1), 0.5, make_shared<metal>(color(.8, .6, .2), 0.0)));
+    world.add(make_shared<sphere>(point3(-1,0,-1), 0.5, make_shared<dielectric>(1.5)));
 
     camera cam;
 
