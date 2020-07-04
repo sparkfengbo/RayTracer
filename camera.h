253c298a0b5445ce2c033af072fbff28a3ab3c87
fengbo.hit <fengbo.hit@bytedance.com>
2020-07-05 00:14:22 +0800

    update 11

--- camera.h
+++ camera.h
@@ -10,7 +10,7 @@
 #include "ray.h"
 
 class camera {
-public:
+private:
     point3 origin;
     point3 lower_left_corner;
     vec3 horizontal;
@@ -28,6 +28,28 @@ public:
         vertical = vec3(0, viewport_height, 0);
         lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
     }
+
+    camera(point3 lookfrom,
+           point3 lookat,
+           vec3   vup,
+           double vfov, // vertical field-of-view in degrees
+           double aspect_ratio ) {
+
+        auto theta = degrees_to_radians(vfov);
+        auto h = tan(theta/2);
+        auto viewport_height = 2.0 * h;
+        auto viewport_width = aspect_ratio * viewport_height;
+
+        auto n = unit_vector(lookfrom - lookat);
+        auto u = unit_vector(cross(vup, n));
+        auto v = cross(n, u);
+
+        origin = lookfrom;
+        horizontal = viewport_width * u;
+        vertical = viewport_height * v;
+        lower_left_corner = origin - horizontal/2 - vertical/2 - n;
+    }
+
 
     ray get_ray(double u, double v) const {
         return  ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);
