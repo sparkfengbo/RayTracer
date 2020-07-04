8053006c86085b70782743add78a177be392d9b7
fengbo.hit <fengbo.hit@bytedance.com>
2020-07-04 18:14:05 +0800

    update  10.4

--- material.h
+++ material.h
@@ -8,6 +8,12 @@
 #include "ray.h"
 #include "hittable.h"
 
+double schlick(double cosine, double ref_idx) {
+    auto r0 = (1-ref_idx) / (1+ref_idx);
+    r0 = r0*r0;
+    return r0 + (1-r0)*pow((1 - cosine),5);
+}
+
 
 class material {
 public:
@@ -69,6 +75,13 @@ public:
         double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
         double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
         if (etai_over_etat * sin_theta > 1.0 ) {
+            vec3 reflected = reflect(unit_direction, rec.normal);
+            scattered = ray(rec.p, reflected);
+            return true;
+        }
+
+        double reflect_prob = schlick(cos_theta, etai_over_etat);
+        if (random_double() < reflect_prob){
             vec3 reflected = reflect(unit_direction, rec.normal);
             scattered = ray(rec.p, reflected);
             return true;
