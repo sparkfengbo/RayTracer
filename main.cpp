8fc22b38d91eb25664752aeac826a48fcedc6a37
fengbo.hit <fengbo.hit@bytedance.com>
2020-07-04 15:14:09 +0800

    update

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ color ray_color(const ray& r, const hittable& world, int depth) {
     if (depth <= 0)
         return color(0,0,0);
 
-    if (world.hit(r, 0, infinity, rec)) {
+    if (world.hit(r, 0.001, infinity, rec)) {
         point3 target = rec.p + rec.normal + random_in_unit_sphere();
         return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
 //        return 0.5 * (rec.normal + color(1,1,1));
