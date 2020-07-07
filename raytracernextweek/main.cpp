2012f5226cd1acc413864f8455808b36298894f5
fengbo.hit <fengbo.hit@bytedance.com>
2020-07-07 21:50:55 +0800

    update 8

--- raytracernextweek/main.cpp
+++ raytracernextweek/main.cpp
@@ -9,6 +9,7 @@
 #include "material.h"
 #include "moving_sphere.h"
 #include "aarect.h"
+#include "box.h"
 
 /**
  * https://raytracing.github.io/books/RayTracingTheNextWeek.html#motionblur
@@ -155,20 +156,34 @@ hittable_list cornell_box() {
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
     objects.add(make_shared<flip_face>(make_shared<xy_rect>(0, 555, 0, 555, 555, white)));
 
+
+    shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
+    box1 = make_shared<rotate_y>(box1,  15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    objects.add(box1);
+
+    shared_ptr<hittable> box2 = make_shared<box>(point3(0,0,0), point3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+    objects.add(box2);
+//    objects.add(make_shared<box>(point3(130, 0, 65), point3(295, 165, 230), white));
+//    objects.add(make_shared<box>(point3(265, 0, 295), point3(430, 330, 460), white));
+
+
     return objects;
 }
 
 int main() {
     const color background(0,0,0);
     const auto aspect_ratio = 16 / 9.;
-    const int image_width = 384;
+    const int image_width = 600;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    auto world = simple_light();
+    auto world = cornell_box();
 
     point3 lookfrom(278, 278, -800);
     point3 lookat(278,278,0);
@@ -194,9 +209,5 @@ int main() {
         }
     }
     std::cerr << "\nDone.\n";
-
-
-
-
     return 0;
 }
